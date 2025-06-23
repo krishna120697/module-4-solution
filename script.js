5de3a2d3fa07b28daf3a4788f1a3008662fea749
Krishna Murari <krishnamission701@gmail.com>
2025-06-23 18:24:24 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// Wrap in an IIFE
+(function () {
+  var names = [
+    "Yaakov", "John", "Jen", "Jason", "Paul",
+    "Frank", "Larry", "Paula", "Laura", "Jim"
+  ];
+
+  // Loop through names
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})();
