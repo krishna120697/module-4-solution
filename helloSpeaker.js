2fc369164b9defc9fbb3576ce4181300cbb622fa
Krishna Murari <krishnamission701@gmail.com>
2025-06-23 18:25:08 +0530

    Create helloSpeaker.js

---
+++ helloSpeaker.js
@@ -0,0 +1,11 @@
+// Wrap in an IIFE and attach to window
+(function (window) {
+  var helloSpeaker = {};
+  var speakWord = "Hello";
+
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+
+  window.helloSpeaker = helloSpeaker;
+})(window);
